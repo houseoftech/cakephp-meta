481e086d30a6ef0123570991c1dc74c2d1a46093
Corie Slate <corie@houseoftech.com>
2014-04-06 09:01:47 -0400

    Support child parent site relationships

--- Model/Metum.php
+++ Model/Metum.php
@@ -2,5 +2,13 @@
 class Metum extends MetaAppModel {
 	var $name = 'Metum';
 	var $useTable = 'meta';
+	var $useDbConfig = 'default';
+	
+	function __construct() {
+		if (defined('CHILD_APP_DIR') && file_exists(ROOT . DS . CHILD_APP_DIR . DS . 'Config' . DS . 'database.php')) {
+			$this->setDataSource('alt');
+		}
+		parent::__construct();
+	}
 }
 ?>
